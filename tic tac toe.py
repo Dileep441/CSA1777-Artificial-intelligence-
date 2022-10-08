b9c117e651ab0f716ca11833b5aad57e29875898
M  Dileep(192111648) <113426680+Dileep441@users.noreply.github.com>
2022-10-08 08:55:23 +0530

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -16,7 +16,7 @@ class TicTacToe:
     def is_player_win(self, player):
         win = None
         n = len(self.board)
-        # checking rows
+     
         for i in range(n):
             win = True
             for j in range(n):
@@ -25,7 +25,7 @@ class TicTacToe:
                     break
             if win:
                 return win
-        # checking columns
+      
         for i in range(n):
             win = True
             for j in range(n):
@@ -34,7 +34,7 @@ class TicTacToe:
                     break
             if win:
                 return win
-        # checking diagonals
+       
         win = True
         for i in range(n):
             if self.board[i][i] != player:
@@ -79,25 +79,24 @@ class TicTacToe:
         while True:
             print(f"Player {player} turn")
             self.show_board()
-            # taking user input
+          
             row, col = list(
                 map(int, input("Enter row and column numbers to fix spot: ").split()))
             print()
-            # fixing the spot
+          
             self.fix_spot(row - 1, col - 1, player)
-            # checking whether current player is won or not
+          
             if self.is_player_win(player):
                 print(f"Player {player} wins the game!")
                 break
-            # checking whether the game is draw or not
+            
             if self.is_board_filled():
                 print("Match Draw!")
                 break
-            # swapping the turn
+           
             player = self.swap_player_turn(player)
-        # showing the final view of board
+        
         print()
         self.show_board()
-# starting the game
 tic_tac_toe = TicTacToe()
 tic_tac_toe.start()
