3f13d75c18629519f095a225f943fde32abbcab0
M  Dileep(192111648) <113426680+Dileep441@users.noreply.github.com>
2022-10-08 12:58:11 +0530

    Create decision tree.py

---
+++ decision tree.py
@@ -0,0 +1,40 @@
+from collections import deque
+ 
+class Graph:
+    def _init_(self, adjac_lis):
+        self.adjac_lis = adjac_lis
+ 
+    def get_neighbors(self, v):
+        return self.adjac_lis[v]
+
+    def h(self, n):
+        H = {
+            'A': 1,
+            'B': 1,
+            'C': 1,
+            'D': 1
+        }
+ 
+        return H[n]
+ 
+    def a_star_algorithm(self, start, stop):
+
+        open_lst = set([start])
+        closed_lst = set([])
+
+        poo = {}
+        poo[start] = 0
+
+        par = {}
+        par[start] = start
+ 
+        while len(open_lst) > 0:
+            n = None
+
+            for v in open_lst:
+                if n == None or poo[v] + self.h(v) < poo[n] + self.h(n):
+                    n = v;
+ 
+            if n == None:
+                print('Path does not exist!')
+                return None
